9c96d4f3e5a1490096cbee705d111ef59140baf5
angeldanylo35 <angeldanylo35@gmail>
2026-09-06 09:54:29 -0300

    vamo

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,26 +2,47 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Usuario extends Model
+class User extends Authenticatable
 {
-    protected $table = 'usuarios';
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
     protected $fillable = [
-        'nome',
+        'name',
         'email',
-        'data_nascimento',
-        'nome_usuario',
-        'senha',
+        'password',
     ];
 
-    // nunca expõe a senha em respostas JSON
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
     protected $hidden = [
-        'senha',
+        'password',
+        'remember_token',
     ];
 
-    protected $casts = [
-        'data_nascimento' => 'date',
-    ];
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
