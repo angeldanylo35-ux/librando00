c6bc0aab0d9cbd0fa9398c3dcc7622f091c22e17
henrique <160766948+henriqu3mart1ns@users.noreply.github.com>
2026-09-13 14:51:38 -0300

    Add files via upload

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/cadastro', [AuthController::class, 'cadastro']);
+Route::get('/verificar-email', [AuthController::class, 'verificarEmail']);
 ?>
